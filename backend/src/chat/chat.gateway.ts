acba6e2002fbfb44035eb3e3532e2bc710edc8b2
아기새 <82822684+liebespaar93@users.noreply.github.com>
2023-06-10 00:36:37 +0900

    done refresh on realtime (#5)
    * done refresh on realtime
    
    * joinRoom name refix
    
    ---------
    
    Co-authored-by: adm <adm@bases-MacBook-Air.local>

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,30 +1,66 @@
-import { ConnectedSocket, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Namespace, Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: 'chat', cors: true })
-export class ChatGateway implements 
-	OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-	@WebSocketServer() server: Namespace;
+  @WebSocketServer() server: Namespace;
 
   afterInit(server: Server) {
-	  console.count('Init');
-	  this.server.server.engine.opts.pingTimeout = 20000;
-	  this.server.server.engine.opts.pingInterval = 20000;
-	  this.server.server.engine.opts.upgradeTimeout = 20000;
-  }
-  handleConnection(@ConnectedSocket() client: Socket, ...args: any[])
-  {
-	  client.join("defult");
-	  client.leave(client.id);
-	  console.log('connect');
-	  console.log(client.id);
-  }
-  
-  handleDisconnect(@ConnectedSocket() client: Socket)
-  {
-	  console.log('disconnect');
-	  console.log(client.id);
+    console.count('Init');
+    this.server.server.engine.opts.pingTimeout = 20000;
+    this.server.server.engine.opts.pingInterval = 20000;
+    this.server.server.engine.opts.upgradeTimeout = 20000;
   }
 
+  handleConnection(@ConnectedSocket() client: Socket, ...args: any[]) {
+    client.join('defult');
+    client.leave(client.id);
+    console.log('connect');
+    console.log(client.id);
+    client.emit('room-refresh', this.ft_room_list());
+  }
+
+  handleDisconnect(@ConnectedSocket() client: Socket) {
+    console.log('disconnect');
+    console.log(client.id);
+  }
+  /**
+   * @name ft_room_refresh
+   * @param client
+   * @param payload
+   * @emits server => "room-list"
+   * @brief 방 리스트 목록 조회
+   */
+  @SubscribeMessage('room-refresh')
+  ft_room_refresh(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() payload: string,
+  ) {
+    client.emit('room-list', this.ft_room_list());
+  }
+
+  /**
+   * @name ft_room_list
+   * @returns room_list : string []
+   * @brief 방 모든 목록을 가져온다
+   */
+  ft_room_list(): string[] {
+    let room_list: string[] = [];
+    this.server.adapter.rooms.forEach((val, key, map) => {
+      console.log(val, key);
+      room_list.push(key);
+    });
+    return room_list;
+  }
 }
--- frontend/src/routes/chat/+page.svelte
+++ frontend/src/routes/chat/+page.svelte
@@ -1,12 +1,24 @@
 <script lang='ts'>
 	import { goto } from "$app/navigation";
 	import { io_chat } from "$lib/webSocketConnection_chat";
+	import { each } from "svelte/internal";
 
-	io_chat;
+	let	rooms_list : string [] = [];
 
-	function ft_goto_chatroom(room_select: string)
+
+	io_chat.on("room-refresh", ( data : string []) => {
+		rooms_list = [...data ]
+	});
+
+
+	function JoinRoom(room_select: string)
 	{
 		goto('/chat/' + room_select);
-	}
+	};
 
 </script>
+<lu>
+{#each rooms_list as room}
+	<li id="room" on:keydown={ () => { JoinRoom(room) } }  > {room} </li>
+{/each}
+</lu>
