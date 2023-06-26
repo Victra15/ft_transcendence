c5e4f0833d12071de7598dab425adfdd4690e120
jinwoole <jinwoole@student.42seoul.kr>
2023-06-26 14:59:23 +0900

    [FEAT] 스타워즈 오프닝 음악  추가 + 프로필 뒤로가기 삭제 (#80)
    Co-authored-by: jinwoole <jinwoolee42@outlook.com>

--- frontend/src/routes/main/[chat_room]/+page.svelte
+++ frontend/src/routes/main/[chat_room]/+page.svelte
@@ -116,9 +116,6 @@
 		ft_chat_send_msg();
 	}
 
-	function ft_exit_chat_room() {
-		goto('/');
-	}
 </script>
 
 <svelte:window on:popstate={() => goto("/main")}/>
@@ -178,9 +175,6 @@
 			/>
 			<button class="variant-filled-primary text_input_btn" on:click={ft_chat_send_msg}>Send</button>
 		</div>
-	</div>
-	<div>
-		<button type="button" on:click={ () => { ft_exit_chat_room()}}   >  뒤로가기 </button>
 	</div>
 </div>
 {/if}
