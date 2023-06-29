304ce55f6029602e1d1d4680496f013d459ff892
Jinho <74135462+Jake1152@users.noreply.github.com>
2023-06-29 22:02:26 +0900

    Sun 104 dm search (#88)
    * [WIP]
    
    - search in DM tab
      - 유저 검색에는 성공 이제 받아온 데이터를 가지고 DmChatStore형식으로 만들어서 localStorage에 추가 필요
    - create chat roomwith trim()
    
    * [FEAT]
    
    - search로 가져온 userInfo 실제 DmChatStore에 타입의 변수로 만들어서 localStorage에 저장
    - dummy data structure dictionary로 변경
    - dummy에서 실제 localStorage에서 가져온 데이터로 변경 시도
    
    TODO
    - dm 소통을 통해 받아와서 localStorage에 저장된 데이터 DM chat UI에도 뿌려주어야한다.
    
    * [CHORE]
    
    - ChatUI.svelte -> DmChatUI.svelte
      호출하는 파일 및 변수 변경
    
    * [WIP]
    
    - Dm part
    
    * [WIP]
    
    - DM chat
    socket on
    
    * [WIP]
    
    - socket on in DmChatUI
    
    * [FEAT]
    
    - DB에 있는 유저를 검색해서 DM list에 추가한다
    - 채팅방 생성시 Enter 키 지원
    
    * [FEAT]
    
    - update DM list when click DM tab

--- frontend/src/components/Chat/DmList.svelte
+++ frontend/src/components/Chat/DmList.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
   export let userInfo: UserDTO
 
-  import { onMount, onDestroy } from 'svelte';
+  import { onMount, onDestroy } from 'svelte'
 
   import type { DmChatStoreIF, DmUserInfoIF } from '$lib/interface'
   import DmUser from './DmUser.svelte'
   import { DM_KEY } from '$lib/webSocketConnection_chat'
   import { getApi } from '../../service/api'
-  // Autocomplete for search
-  // import { Autocomplete } from '@skeletonlabs/skeleton'
-  // import type { AutocompleteOption } from '@skeletonlabs/skeleton'
 
   let opponentUserId= ''
   let loadDmChat : string | null
@@ -17,52 +14,35 @@
   // 컴포넌트가 DOM에 마운트될 때이니 굳이 인가?
   let dmStoreData : DmChatStoreIF = {}
   $: dmStoreData
-  /*
-      - await 참고용
-      - 1.
-          const updateFriend = async (): Promise<void> => {
-              if (toggleRefresh)
-              {
-              friendList = await getApi({
-                      path: 'friends/',
-                  })
-              console.log("done")
-              }
-          }
-      - 2.
-          onMount (async () => {
-              try {
-                  // dmChatStore[opponent]._userInfo = await getApi({
-                  //     path: 'user/' + opponent,
-                  // })
-
-              } catch (error )
-              {
-                  alert('오류 : ' + opponent + ' user정보를 가져올 수 없습니다.')
-                  // await goto('/main')k
-              }
-          })
-  */
 
   /**
-   * dmStore에 있는 모든 유저들의 userInfo를 최신화 한다.
-   * getApi로 각 유저의 userInfo를 가져온 뒤 덮어쓰게 한다
-   * 더욱 나은 방법은 getApi userInfo를 가져온 뒤 바뀐 부분이 있는지 비교하여 해당 부분만 바꾼다.
-   * 또한 DM list에 있는 유저를 한정적으로 가져간다.
+   * 실제로  dmStoreData[key]._userInfo = curUserInfo 코드가 동작하는지 확인이 필요하다
+   * 유저가 프로필이나 닉네임을 바꾸어보고 적용되는지 확인해야한다.
+   * 
    */
-  function ft_update_dm_list()  {
-
+  // feat GPT for .. each solution detach from Ojbect
+  async function ftUpdateDmList(): Promise<void> {
+    try {
+      for (const key of Object.keys(dmStoreData)) {
+        const curUserInfo: UserDTO | null = await getApi({ path: 'user/' + key })
+        dmStoreData[key]._userInfo = curUserInfo
+      }
+    } catch (error) {
+      alert('오류: 사용자 정보를 가져올 수 없습니다.')
+    }
   }
+
   // await가 필요한 케이스인가?, 함수호출에만 쓰는가?
   // 브라우저 local storage에서 바로 가져오니 필요 없는가?
   // 이미 이 순간에 업데이트 하게 한다.
-  // tab이 선택되는 순간에 mount되는것인가?
+  // tab이 선택되는 순간에 mount되는것인가? 그러하다
   onMount(async () => {
       try {
         loadDmChat = localStorage.getItem(DM_KEY)
-        if (loadDmChat)
+        if (loadDmChat) {
           dmStoreData = JSON.parse(loadDmChat)
-        
+          ftUpdateDmList()
+        }
       } catch (error) {
         console.log('DM loading error')
       }
@@ -74,20 +54,7 @@
       성능상의 이슈는 없는가?
   */
   function ftUpdateChatLocalStorage(newDmChatStore : DmUserInfoIF) {
-      // let dmData : DmChatStoreIF = {}
-      // if (loadDmChat)
-      //     dmData = JSON.parse(loadDmChat)
-      // console.log("============= ftUpdateChatLocalStorage =============")
-      // console.log("opponentUserId : " + opponentUserId)
-      // console.log("before assign dmStoreData : ")
-      // console.log(dmStoreData)
-      // console.log("newDmChatStore : ")
-      // console.log(newDmChatStore)
       dmStoreData[opponentUserId] = newDmChatStore // deep copy가 될것인가? 혹시 꼬일 부분은 없는가?
-      // console.log("after assign dmStoreData : ")
-      // console.log(dmStoreData)
-      // console.log("dmStoreData[opponentUserId] : ")
-      // console.log(dmStoreData[opponentUserId])
       localStorage.setItem(DM_KEY, JSON.stringify(dmStoreData))
   }
 
@@ -108,16 +75,11 @@
         else if (opponentUserId === userInfo.id) {
           return alert('DM 대상으로 자신을 추가할 수는 없습니다.')
         }
-        // TODO 이미 추가된 유저인지 확인 필요
-        // else if () {
-          
-        // }
-        // getApi()로 받아온 것을 바로 할당하는 것이 가능할 것인가?
-        
+        else if (opponentUserId in dmStoreData) { // TODO 이미 추가된 유저인지 확인 필요
+          return alert('이미 ' + opponentUserId + '은(는) DM 대상으로 등록되었습니다.')
+        }
         console.log("opponentUserId : " + opponentUserId)
         let searchedUser : UserDTO | null = await getApi({ path: 'user/' + opponentUserId})
-        // console.log(typeof true);
-        // || searchedUser === undefined
         if (typeof searchedUser === "string" || searchedUser === null || searchedUser === undefined)
         {
           console.log("searchedUser : ")
@@ -134,6 +96,7 @@
           alert('오류 : ' + opponentUserId + ' user정보를 가져올 수 없습니다.')
       }
   }
+
 </script>
 
 <div>
--- frontend/src/components/Chat/DmUser.svelte
+++ frontend/src/components/Chat/DmUser.svelte
@@ -2,30 +2,56 @@
     export let opponent: string
     export let dmUserInfo: DmUserInfoIF
     export let userInfo: UserDTO
+    $: dmUserInfo
 
     import type { DmUserInfoIF } from '$lib/interface'
     import DmChatUI from "./DmChatUI.svelte"
     import { Avatar, modalStore } from '@skeletonlabs/skeleton'
     import type { ModalComponent, ModalSettings } from '@skeletonlabs/skeleton'
-    import { getApi } from '../../service/api'
+    // import { getApi } from '../../service/api'
     
     // To check 데이터구조 한 칸 더 안으로 들어갔으므로 제대로 key값을 가져오는지 확인 필요.
 
     /**
      * 실제로 api요청해서 데이터 가져올때 필요 
      * userInfo 최신화시점. DM list tab에서 유저에 의해 click될때.
+     * 실패해버림
+     * modal이 제대로 동작하지 않는 이슈 발생
+     * 해결 방법은 modal에서도 되도록 어떻게든 찾아서 하거나
+     * DM chat UI 안에서만 갱신하거나
+     * 현재 DM tab 클릭했을 때 갱신하므로 그때 하는 것으로 놔두거나
+     * DM tab 자체도 5초마다 갱신하게 하거나 등등이 있다.
+     * DM tab list에 있는 모든 유저에 대해 갱신하는 것이 맘에 안드므로 바꾸려면 DB를 쓰거나 다른 방식이 필요할 듯 싶다
      */
-    // async function getUserInfo(): Promise<void> {
-    //     userInfo = await getApi({
-    //         path: 'user/' + DmUserInfo.opponent,
-    //     })
-    // }
+    /**
+     * async function ftUpdateDmUser(): Promise<void> {
+        try {
+            dmUserInfo = await getApi({ path: 'user/' + opponent})
+            if (typeof dmUserInfo === "string")
+                return alert(opponent + ' user정보를 가져올 수 없습니다.')
+            console.log("dmUserInfo : ")
+            console.log(dmUserInfo)
+        } catch (error) {
+            alert('오류 : ' + opponent + ' user정보를 가져올 수 없습니다.')
+        }
+    }
+    */
 
     /*
         - click했을때 DM창 뿌려주게 처리 필요
             DmUserInfo data 를 user id를 가지고 가져올 필요가 있다.
+        - async를 호출하는 쪽에서도 항상 적용해야하는가?
+        타고타고 가야하는가?
+        a() -> b() -> c()
+        b에서 c를 호출하는데 c()가 async일 필요가 있다면
+        a,b,c모두 async여야 하는가?
+        어쨋든 modal 호출하는 함수에 다른거 껴넣으면 modal에서 문제 발생하여 쓸 수 없음
     */
     function ftDmChat() {
+        
+        /*
+            update userInfo 
+        */
         const modalComponent: ModalComponent = {
             ref: DmChatUI,
             props: {opponent: opponent,
@@ -54,8 +80,9 @@
         bind:src={dmUserInfo._userInfo.avatar}
         width="w-7"
         rounded="rounded-full"
-        />
+    />
     <span class="flex-auto">
+        <!-- 프로필에서 변경한 nickname이 잘 적용되는지 확인 필요-->
         <dt>
             {opponent} | {dmUserInfo._userInfo.nickname}
         </dt>
